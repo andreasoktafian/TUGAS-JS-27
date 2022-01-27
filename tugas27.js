551ae918c903b6e4fd4c44932608f13d8b24447f
andreas <andreas.oktafiann@gmail.com>
2022-01-27 14:28:40 +0700

    TUGAS JS 27

---
+++ tugas27.js
@@ -0,0 +1,17 @@
+function panggil() {
+  var a = "No";
+  var b = " rumah saya ";
+  var c = 30;
+  var d = "A";
+
+  var result = a + b + String(c) + d;
+  console.log(result);
+
+  var e = "Jalan pangeran Tirtayasa No ";
+  var f = 70;
+
+  var result2 = e + String(f);
+  console.log(result2);
+}
+
+panggil();
